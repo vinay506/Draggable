097d5c8e6fa92fe59731665c59f3c09650a333ed
vinay <vinay.bommali@xcubelabs.com>
2020-06-12 09:58:49 +0530

    Minor code changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,14 +12,21 @@ import * as $ from 'jquery';
 })
 export class AppComponent implements OnInit {
   title = 'DraggCols';
+
+  colsList = ["sampleCard", "sampleCard1", "sampleCard2", "sampleCard3", "sampleCard4", "sampleCard5"];
+  currentlyDragging= '';
+
   ngOnInit() {
     this.initialize()
   }
+  
+  drag(event){
+    this.currentlyDragging = (<HTMLDivElement>event.target).id;
+  }
 
- 
   initialize() {
 
-    $(document).ready(function () {
+    $(document).ready(() => {
       var ele1 = document.getElementById("sampleCard1");
       var ele = document.getElementById("sampleCard");
       var ele2 = document.getElementById("sampleCard2");
@@ -28,58 +35,81 @@ export class AppComponent implements OnInit {
       var ele5 = document.getElementById("sampleCard5");
 
 
-      ele.addEventListener('mousedown',function (event) {
+      ele.addEventListener('dragstart',(event)=>{
+        this.drag(event);
+      })
+
+      ele1.addEventListener('dragstart',(event)=>{
+        this.drag(event);
+      })
+
+      ele2.addEventListener('dragstart',(event)=>{
+        this.drag(event);
+      })
+
+      ele3.addEventListener('dragstart',(event)=>{
+        this.drag(event);
+      })
+
+      ele4.addEventListener('dragstart',(event)=>{
+        this.drag(event);
+      })
+      ele5.addEventListener('dragstart',(event)=>{
+        this.drag(event);
+      })
+
+      ele.addEventListener('mousedown', function (event) {
         const targetEle = event.target;
-        $(targetEle).attr('draggable','true')
+        $(targetEle).attr('draggable', 'true')
       });
-      ele.addEventListener('mouseup',function (event) {
+      ele.addEventListener('mouseup', function (event) {
         const targetEle = event.target;
         $(targetEle).removeAttr('draggable')
       });
 
-      ele1.addEventListener('mousedown',function (event) {
+      ele1.addEventListener('mousedown', function (event) {
         const targetEle = event.target;
-        $(targetEle).attr('draggable','true')
+        $(targetEle).attr('draggable', 'true')
       });
-      ele1.addEventListener('mouseup',function (event) {
-        const targetEle = event.target;
-        $(targetEle).removeAttr('draggable')
-      });
-
-
-      ele2.addEventListener('mousedown',function (event) {
-        const targetEle = event.target;
-        $(targetEle).attr('draggable','true')
-      });
-      ele2.addEventListener('mouseup',function (event) {
+      ele1.addEventListener('mouseup', function (event) {
         const targetEle = event.target;
         $(targetEle).removeAttr('draggable')
       });
 
 
-      ele3.addEventListener('mousedown',function (event) {
+      ele2.addEventListener('mousedown', function (event) {
         const targetEle = event.target;
-        $(targetEle).attr('draggable','true')
+        $(targetEle).attr('draggable', 'true')
       });
-      ele3.addEventListener('mouseup',function (event) {
-        const targetEle = event.target;
-        $(targetEle).removeAttr('draggable')
-      });
-
-      ele4.addEventListener('mousedown',function (event) {
-        const targetEle = event.target;
-        $(targetEle).attr('draggable','true')
-      });
-      ele4.addEventListener('mouseup',function (event) {
+      ele2.addEventListener('mouseup', function (event) {
         const targetEle = event.target;
         $(targetEle).removeAttr('draggable')
       });
 
-      ele5.addEventListener('mousedown',function (event) {
+
+      ele3.addEventListener('mousedown', function (event) {
         const targetEle = event.target;
-        $(targetEle).attr('draggable','true')
+        $(targetEle).attr('draggable', 'true')
       });
-      ele5.addEventListener('mouseup',function (event) {
+      ele3.addEventListener('mouseup', function (event) {
+        const targetEle = event.target;
+        $(targetEle).removeAttr('draggable')
+      });
+
+      ele4.addEventListener('mousedown', function (event) {
+        const targetEle = event.target;
+        $(targetEle).attr('draggable', 'true')
+      });
+      ele4.addEventListener('mouseup', function (event) {
+        const targetEle = event.target;
+        $(targetEle).removeAttr('draggable')
+      });
+
+      ele5.addEventListener('mousedown', function (event) {
+        const targetEle = event.target;
+        $(targetEle).attr('draggable', 'true')
+      });
+      ele5.addEventListener('mouseup', function (event) {
         const targetEle = event.target;
         $(targetEle).removeAttr('draggable')
       });
@@ -89,98 +119,109 @@ export class AppComponent implements OnInit {
        * 
        */
 
-      ele.addEventListener("dragend", function (event) {      
-        onDragEnd(event);
+      ele.addEventListener("dragend", (event) => {
+        this.onDragEnd(event);
       });
 
-      ele1.addEventListener("dragend", function (event) {      
-        onDragEnd(event);
+      ele1.addEventListener("dragend", (event) => {
+        this.onDragEnd(event);
       });
 
-      ele2.addEventListener("dragend", function (event) {      
-        onDragEnd(event);
+      ele2.addEventListener("dragend", (event) => {
+        this.onDragEnd(event);
       });
 
-      ele3.addEventListener("dragend", function (event) {      
-        onDragEnd(event);
+      ele3.addEventListener("dragend", (event) => {
+        this.onDragEnd(event);
       });
 
-      ele4.addEventListener("dragend", function (event) {      
-        onDragEnd(event);
+      ele4.addEventListener("dragend", (event) => {
+        this.onDragEnd(event);
       });
 
-      ele5.addEventListener("dragend", function (event) {      
-        onDragEnd(event);
-      });
-
-      
-        /**
-       *  Add Drag enter event to elements
-       * 
-       */
-
-
-      ele.addEventListener("dragenter", function (event) {
-        onDragEnter(event);
-      });
-
-      ele1.addEventListener("dragenter", function (event) {
-        onDragEnter(event);
+      ele5.addEventListener("dragend", (event) => {
+        this.onDragEnd(event);
       });
 
 
-      ele2.addEventListener("dragenter", function (event) {
-        onDragEnter(event);
-      });
+      /**
+     *  Add Drag enter event to elements
+     * 
+     */
 
-      ele3.addEventListener("dragenter", function (event) {
-        onDragEnter(event);
-      });
 
-      ele4.addEventListener("dragenter", function (event) {
-        onDragEnter(event);
-      });
+      ele.addEventListener("dragenter", (event) => {
+        this.onDragEnter(event);
+      },true);
 
-      ele5.addEventListener("dragenter", function (event) {
-        onDragEnter(event);
-      });
+      ele1.addEventListener("dragenter", (event) => {
+        this.onDragEnter(event);
+      },true);
 
-     
-     
-     
-     
-   
+
+      ele2.addEventListener("dragenter", (event) => {
+        this.onDragEnter(event);
+      },true);
+
+      ele3.addEventListener("dragenter", (event) => {
+        this.onDragEnter(event);
+      },true);
+
+      ele4.addEventListener("dragenter", (event) => {
+        this.onDragEnter(event);
+      },true);
+
+      ele5.addEventListener("dragenter", (event) => {
+        this.onDragEnter(event);
+      },true);
+
+
+
+
 
     });
   }
-}
 
 
-
-function onDragEnd(event) {
-  let targetEle = event.target;
-  const offset = $(targetEle).offset();
-  const offsetWidth = (<HTMLDivElement>targetEle).offsetWidth;
-  const pos = event.clientX - offsetWidth / 2;
-  $(targetEle).offset({ left: pos });
-}
-
-
-function onDragEnter(event) {
-
-  var targetEle = event.target;
-  var offset = $(targetEle).offset();
-  var offsetWidth = (<HTMLDivElement>targetEle).offsetWidth;
-  var targeOffset = offset.left + offsetWidth / 2;
-
-  if (event.clientX < targeOffset) {
-    const pos = offset.left - offsetWidth - offsetWidth / 2 - 30
+  onDragEnd(event) {
+    let targetEle = event.target;
+    const offset = $(targetEle).offset();
+    const offsetWidth = (<HTMLDivElement>targetEle).offsetWidth;
+    const pos = event.clientX - offsetWidth / 2;
     $(targetEle).offset({ left: pos });
+  }
 
-  } else if (event.clientX > targeOffset) {
-    const pos = offset.left + offsetWidth + offsetWidth / 2 + 30
-    $(targetEle).offset({ left: pos });
+
+  onDragEnter(event) {
+    var targetEle = event.target;
+    if (this.isColCard()) {
+
+      var offset = $(targetEle).offset();
+      var offsetWidth = (<HTMLDivElement>targetEle).offsetWidth;
+      var targeOffset = offset.left + offsetWidth / 2;
+
+      if (event.clientX < targeOffset) {
+        const pos = offset.left - offsetWidth - offsetWidth / 2 - 30
+        $(targetEle).offset({ left: pos });
+
+      } else if (event.clientX > targeOffset) {
+        const pos = offset.left + offsetWidth + offsetWidth / 2 + 30
+        $(targetEle).offset({ left: pos });
+
+      }
+
+    }
 
   }
 
+  isColCard() {
+   
+    return this.colsList.some(item => {
+      return (item === this.currentlyDragging);
+    })
+  }
 }
+
+
+
+
